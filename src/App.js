1b7d83754bc8df76f0bed24a8cda2454626c8e39
Ali Babolhavaeji <ababolha@cccis.com>
2022-04-29 13:42:27 -0500

    Add getting access to camera

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Webcam from "react-webcam";
+
+const cameraWidth = 720;
+const cameraHeight = 720;
+const aspectRatio = cameraWidth / cameraHeight;
+const videoConstraints = {
+  width: {
+    min: cameraWidth,
+  },
+  height: {
+    min: cameraHeight,
+  },
+  aspectRatio,
+};
 
 function App() {
   return (
@@ -7,6 +21,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>This is created by Ali.bhji</p>
+        <Webcam
+          videoConstraints={videoConstraints}
+          width={cameraWidth}
+          height={cameraHeight}
+        />
       </header>
     </div>
   );
